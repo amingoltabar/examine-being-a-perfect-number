ee55051161e2d0a90dc7e07eca939f45efddbc9e
amingoltabar <61889677+amingoltabar@users.noreply.github.com>
2020-03-20 18:28:38 +0330

    Add files via upload

---
+++ p25.py
@@ -0,0 +1,22 @@
+while True:
+    s=0
+    a=int(input('enter the number '))
+    for i in range(1,a,1):
+          if (a%i)==0:
+              s+=i
+    if s==a:
+          print('this number is perfect')
+    else:
+        print('this number is not perfect')
+        
+        
+          
+    
+          
+             
+    
+    
+          
+    
+    
+    
